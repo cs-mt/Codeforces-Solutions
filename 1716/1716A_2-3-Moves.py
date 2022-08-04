ce7bb5dd69bc8adc38de1b2addf81b5a03d116c2
cs-mt <110451185+cs-mt@users.noreply.github.com>
2022-08-04 19:26:37 +0300

    Create 1716A_2-3-Moves.py

---
+++ 1716/1716A_2-3-Moves.py
@@ -0,0 +1,27 @@
+test_cases = int(input())
+
+while test_cases > 0:
+    test_cases -= 1
+    
+    case = int(input())
+
+    if case % 3 == 0:
+        print(int(case//3))
+        continue
+    elif case % 3 == 1:
+        step3 = int(case//3)+2
+        if case % 2 == 0 and case // 2 < step3:
+            print(case//2)
+        else:
+            if case == 1:
+                print(2)
+                continue 
+
+            if (case - ((case//3-1) * 3)) % 2 == 0 and ((case - ((case//3-1) * 3))) // 2 + (case//3-1) < step3 and ((case - ((case//3-1) * 3))) + ((case//3-1) * 3) == case:
+                print(((case - ((case//3-1) * 3)) // 2) + (case//3-1))
+            else:
+                print(step3)
+        continue
+    elif case % 3 == 2:
+        print(int(case//3)+1)
+        continue
