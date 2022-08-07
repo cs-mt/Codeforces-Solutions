3451f58e4f4dfee000a68d2a67f0ff759a131752
cs-mt <110451185+cs-mt@users.noreply.github.com>
2022-08-07 21:53:54 +0300

    Create 69A_Young-Physicist.py

---
+++ 2011-03-22 #69/69A_Young-Physicist.py
@@ -0,0 +1,22 @@
+# Solved on 2022-08-07 at 21:50 GMT +3 
+# https://codeforces.com/contest/69/problem/A
+
+vector_count = int(input())
+
+vectors = []
+
+x = 0
+y = 0
+z = 0
+
+while vector_count > 0:
+    vector_count -= 1
+    vector = list(map(int, input().split(" ")))
+    x += vector[0]
+    y += vector[1]
+    z += vector[2]
+
+if(x != 0 or y != 0 or z != 0):
+    print("NO")
+else:
+    print("YES")
