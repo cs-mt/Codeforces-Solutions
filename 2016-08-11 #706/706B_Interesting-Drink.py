4f13ffce41669dff11b232ffe9f788d15f736ec6
cs-mt <110451185+cs-mt@users.noreply.github.com>
2022-08-15 06:26:58 +0300

    Create 706B_Interesting-Drink.py

---
+++ 2016-08-11 #706/706B_Interesting-Drink.py
@@ -0,0 +1,32 @@
+# Solved on 2022-08-15 at 06:24 GMT +3
+# https://codeforces.com/contest/706/problem/B
+
+import bisect
+
+n = int(input())
+     
+prices = list(map(int, input().split(" ")))
+
+prices2 = sorted(prices)
+
+days = int(input())
+
+counts = {}
+
+for x in prices2:
+    if x in counts:
+        counts[x] += 1
+    else:
+        counts[x] = 1
+
+
+while days > 0:
+    days -= 1
+    money = int(input())
+
+    location = bisect.bisect_left(prices2, money)
+
+    if money in counts:
+      location += counts[money]
+
+    print(location)
