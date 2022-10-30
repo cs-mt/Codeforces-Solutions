d3753f59e35dea7e2325455088a40f682bbe7f4e
cs-mt <110451185+cs-mt@users.noreply.github.com>
2022-10-30 23:22:16 +0300

    Create 1740A_Factorise-N+M.py

---
+++ 2022-10-29 #1740/1740A_Factorise-N+M.py
@@ -0,0 +1,33 @@
+t = int(input())
+
+def Prime(n):
+    if n == 2:
+        return True
+    if n & 1 == 0:
+        return False
+    d= 3
+    while d * d <= n:
+        if n % d == 0:
+            return False
+        d= d + 2
+    return True
+
+
+
+while t > 0:
+    t -= 1
+
+    first = int(input()) # first prime
+    m = 2
+    # n + m must not be prime
+    # m must be prime
+    # n is prime
+    sum = first+m
+
+    while True:
+        if not Prime(sum) and Prime(m):
+            print(m)
+            break
+        else:
+            m+=1
+            sum = first+m
