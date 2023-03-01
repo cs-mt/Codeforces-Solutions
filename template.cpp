92c4252138f51081001a97f900ff1e99b996ea42
cs-mt <110451185+cs-mt@users.noreply.github.com>
2023-03-01 11:21:58 +0300

    template

---
+++ template.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define OPTIMIZE(); ios::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+
+int main(){
+    OPTIMIZE();
+
+    int t;
+    cin >> t;
+
+    while(t>0){
+        --t;
+
+    }
+}
